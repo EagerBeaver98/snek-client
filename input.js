d164bd67c5aac04d5d66963a764b2807f4a5ef1c
EagerBeaver98 <jooke98@outlook.com>
2020-11-05 16:46:17 +0000

    Movement working

--- input.js
+++ input.js
@@ -1,12 +1,27 @@
-const setupInput = function() {
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  } else if (data === "w") {
+    connection.write("Move: up");
+  } else if (data === "a") {
+    connection.write("Move: left");
+  } else if (data === "d") {
+    connection.write("Move: right");
+  } else if (data === "s") {
+    connection.write("Move: down");
+  }
+};
+
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
+  
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
+  stdin.on('data', (data) => {
+    handleUserInput(data);
   });
   return stdin;
 };
