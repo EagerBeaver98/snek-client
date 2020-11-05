81450aaf7b1351d97942a4027218f3823e4b3ba1
EagerBeaver98 <jooke98@outlook.com>
2020-11-05 00:03:27 +0000

    Takes keyboard input, can kill with crtl + C

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
-
 const { connect } = require('./client');
 console.log("Connecting ...");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+  return stdin;
+};
+
+const handleUserInput = () => {
+  process.exit();
+};
+
+setupInput(handleUserInput);
